4c4c70de8b572554235712b444b809aa5e79bcd8
BLNDZ <37784309+KEMALBALANDIZ@users.noreply.github.com>
2020-02-09 23:45:42 +0000

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,7 @@
+# This notebook was created as a requirement of the Applied Data Science Capstone peer graded assignment
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
