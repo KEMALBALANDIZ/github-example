f4635f7dc51a133a66820f2aa51b1e50e6fc5757
BLNDZ <37784309+KEMALBALANDIZ@users.noreply.github.com>
2020-02-10 00:22:57 +0000

    Delete python_code.py

--- python_code.py
+++
@@ -1,7 +0,0 @@
-# This notebook was created as a requirement of the Applied Data Science Capstone peer graded assignment
-
-
-import pandas as pd
-import numpy as np
-
-print("Hello Capstone Project Course!")
